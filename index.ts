d6c213cb31035efccfad18f2a5bcd53c2a5bf669
Ashwin Talwalkar <atalwalkar719@gmail.com>
2024-09-12 22:49:15 -0500

    done ready for prod. LGTM

--- index.ts
+++ index.ts
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Rate limiter configuration
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
+  max: 25, // Limit each IP to 25 requests per windowMs
   message: { error: 'Too many requests, please try again later.', errorType: 'RATE_LIMIT_EXCEEDED' }
 });
 
